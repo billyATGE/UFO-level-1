8b3879cd7abf540ed8a13e7a9648b0f2f6750487
billyATGE <46874350+billyATGE@users.noreply.github.com>
2021-05-13 22:18:27 -0400

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,19 @@
 var tableData = data;
 
-// HTML elements/nodes
-var filterBtn = d3.select('#filter-btn');
-
-
-// Call the create table function to build the table and insert data into the table.
+//Create table here
 createTable(tableData);
 
 
-
-filterBtn.on('click', () => {
-    var inputElement1 = d3.select("#datetime");
-    var inputValue = inputElement1.property("value");
+//Filter on button click here
+d3.select('#filter-btn').on('click', () => {
+    var inputValue = d3.select("#datetime").property("value");
     var filteredData = tableData.filter(function(event){
         if(inputValue !== null && inputValue !== ''){
             return event.datetime === inputValue;};
           return event.datetime;
         });
     console.log(filteredData)
+    //Use filtered Data to build new table
     createTable(filteredData);
 });
 
